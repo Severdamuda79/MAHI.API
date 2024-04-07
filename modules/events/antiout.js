9914b2c704a27eff7b9c5c2c3665a699b516e353
Abdul-khan-parihar <165261350+Abdul-khan-parihar@users.noreply.github.com>
2024-04-07 14:51:06 +0530

    Update antiout.js

--- modules/events/antiout.js
+++ modules/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "DungUwU",
+ credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
  description: "Listen events"
 };
 
@@ -11,12 +11,14 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (data.antiout == false) return;
  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "leave by itself" : "kicked by the administrator";
- if (type == "data attechment") {
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "Koi Ase Pichware Mai Lath Marta Hai?";
+ if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`𝗡𝗮𝗵𝗶.. 𝗞𝗮𝗿 𝗥𝗮𝗵𝗶 𝗔𝗯 𝗠𝗲 𝗧𝘂𝗷𝗵𝗲 𝗔𝗱𝗱 ${name} 𝗝𝗮 𝗦𝗶𝗺𝗿𝗮𝗻 𝗝𝗲𝗲 𝗟𝗲 𝗔𝗽𝗻𝗶 𝗭𝗶𝗻𝗱𝗮𝗴𝗶 😁 `, event.threadID)
-   } else api.sendMessage(`𝗟𝗼 𝗣𝗵𝗶𝗿 𝗔𝗱𝗱 𝗞𝗮𝗿 𝗗𝗶𝘆𝗮 𝗧𝘂𝗺𝗲 😏 ${name} 𝗕𝗮𝘁𝗮𝗼 𝗞𝘆𝗮 𝗨𝗸𝗵𝗮𝗱 𝗟𝗼 𝗚𝗲 𝗠𝗲𝗿𝗮 .😹 `, event.threadID);
+    api.sendMessage(`${name} Ko Dubara Add Nhi Kar Paya 🥺 Group Mai :( `, event.threadID)
+   } else api.sendMessage(`🍒🎀𝐌𝐞𝐫𝐞 𝐑𝐚𝐡𝐭𝐞 𝐇𝐮𝐲𝐞⛵𝐆𝐫𝐨𝐮𝐩 𝐒𝐞 𝐍𝐚𝐡𝐢 𝐉𝐚 𝐒𝐚𝐤𝐭𝐞😡💫 \n\n
+  🖤 ${name} 🖤
+  \n\n🌸 𝐀𝐝𝐦𝐢𝐧 😡 𝐢𝐬𝐞 𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 𝐝𝐞𝐝𝐨 𝐦𝐚𝐢𝐧𝐞 𝐀𝐝𝐝 𝐤𝐫 𝐝𝐢𝐲𝐚..🙃😅`, event.threadID);
   })
  }
 }
