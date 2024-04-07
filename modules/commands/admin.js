6324cd473ef6d7b8429025c65061d500da1f88be
Abdul-khan-parihar <165261350+Abdul-khan-parihar@users.noreply.github.com>
2024-04-07 12:06:56 +0530

    Update admin.js

--- modules/commands/admin.js
+++ modules/commands/admin.js
@@ -20,13 +20,13 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://i.ibb.co/hfNnxYN/b00aff06f18d6053e3d8ed45058b4c68.jpg", "https://i.ibb.co/jgk6rB0/7ce2390636d1a46a331662dbaaf693d6.jpg", "https://i.ibb.co/qRmVPbx/c2d197fceca703f0b167adf4a4e31821.jpg"
+"https://i.imgur.com/fymeTq9.jpeg", "https://i.imgur.com/fymeTq9.jpeg", "https://i.imgur.com/fymeTq9.jpeg"
     ];
      var callback = () => api.sendMessage({body:`❤️𝐀𝐃𝐌𝐈𝐍 𝐈𝐍𝐅𝐎❤️
 
   🍒𝗠𝗥.. 𝐀𝐁𝐃𝐔𝐋 𝐊𝐇𝐀𝐍🥀•
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com/MeiN.SabKa.MeRa.Koi.Nhi.Abdul?mibextid=ZbWKwL : 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com/ghg.hhgkh?mibextid=ZbWKwL : 
 
 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 : 9634XXX786`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
